6537ec94049785d7cb794ea409465066f2ca7d35
ShabeebaBanu <banumrmrs.21@itfac.mrt.ac.lk>
2024-11-15 20:53:26 +0530

    OrderService version 1

--- order-service/src/main/java/com/onlineshop/order_service/controller/OrderController.java
+++ order-service/src/main/java/com/onlineshop/order_service/controller/OrderController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/order")
+@RequestMapping("/api/v1/order")
 @RequiredArgsConstructor
 public class OrderController {
 
@@ -28,4 +28,5 @@ public class OrderController {
     public List<OrderResponse> getOrders() {
         return orderService.getOrders();
     }
+
 }

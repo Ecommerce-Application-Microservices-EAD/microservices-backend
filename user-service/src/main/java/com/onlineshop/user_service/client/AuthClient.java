27c451d20bea53bf52e0b609e50fa76423050f9d
Madhura Jayashanka <madhuraweerasooriye@gmail.com>
2024-12-08 19:28:19 +0530

    service updated with @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})   [skip ci]

--- user-service/src/main/java/com/onlineshop/user_service/client/AuthClient.java
+++ user-service/src/main/java/com/onlineshop/user_service/client/AuthClient.java
@@ -1,8 +1,10 @@
 package com.onlineshop.user_service.client;
 
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.HttpExchange;
 import org.springframework.web.service.annotation.PostExchange;
 
+@HttpExchange("${auth.url}")
 public interface AuthClient {
     @PostExchange("/api/auth/token")
     String getToken(@RequestParam("username") String username, @RequestParam("role") String role);

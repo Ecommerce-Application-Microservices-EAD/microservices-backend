9466f92f13bb461eb8802dce98782772cd0d29dd
Madhura Jayashanka <madhuraweerasooriye@gmail.com>
2024-12-08 11:05:08 +0530

    restructured

--- docker/postgres/init.sql
+++
@@ -1,5 +0,0 @@
-SELECT 'CREATE DATABASE inventory_service'
-    WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'inventory_service')\gexec
-
-SELECT 'CREATE DATABASE order_service'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'order_service')\gexec
--- payment-service/src/main/java/com/onlineshop/payment_service/config/OpenAPIConfig.java
+++ payment-service/src/main/java/com/onlineshop/payment_service/config/OpenAPIConfig.java
@@ -1,5 +1,4 @@
-package com.onlineshop.order_service.config;
-
+package com.onlineshop.payment_service.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -11,11 +10,11 @@ import org.springframework.context.annotation.Configuration;
 public class OpenAPIConfig {
 
     @Bean
-    public OpenAPI orderServiceAPI() {
+    public OpenAPI paymentServiceAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("Order Service API")
-                        .description("Order Service API for Online Shop")
+                        .title("Payment Service API")
+                        .description("Payment Service API for Online Shop")
                         .version("v1.0.0")
                         .license(new License()
                                 .name("Apache 2.0")
